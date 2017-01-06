72124342d3538daa3bf4de4cd8405804b66185da
arpentnoir <namlleps.drahcir@gmail.com>
2017-01-07 10:22:15 +1100

    create triangletype method

--- src/main/java/io/swagger/Utils.java
+++ src/main/java/io/swagger/Utils.java
@@ -1,5 +1,6 @@
 package io.swagger;
 
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 /**
@@ -12,7 +13,7 @@ public class Utils {
     if(n == 0){
       return 0;
     } else if(n < 0){
-      return -1 * fibonacci(n * -1);
+      return -1 * (int) Math.pow(-1, n) * fibonacci(n * -1);
     } else if(n < 3){
       return 1;
     }
@@ -50,11 +51,54 @@ public class Utils {
   }
 
   public static String triangleType(int a, int b, int c){
+    int[] array = {a, b, c};
+    Arrays.sort(array);
+
+    if(a <= 0 || b <= 0 || c <= 0){
+      return "\"Error\"";
+    } else if(array[2] > array[0] + array[1]){
+      return "\"Error\"";
+    } else if (a == b & a == c){
+      return "\"Equilateral\"";
+    } else if(a != b & a != c & b != c){
+      return "\"Scalene\"";
+    } else if(a == b || a == c || b == c){
+      return "\"Isosceles\"";
+    }
+
     return "";
   }
 
 //  public static void main(String[] args){
+//    System.out.println(fibonacci(5));
+//    System.out.println(fibonacci(-5));
+//    System.out.println(fibonacci(6));
+//    System.out.println(fibonacci(-6));
+//
+//    System.out.println(triangleType(0, 0, 0));
+//    System.out.println(triangleType(1, 1, 2));
+//    System.out.println(triangleType(-1, 0, 1));
+//    System.out.println(triangleType(-1, -1, -1));
+//    System.out.println(triangleType(-1, 1, 1));
+//    System.out.println(triangleType(1, -1, 1));
+//    System.out.println(triangleType(1, 1, -1));
+//    System.out.println(triangleType(3, 3, 5000));
+//    System.out.println(triangleType(1, 1, 2147483647));
+//    System.out.println(triangleType(-2147483648, -2147483648, -2147483648));
+//    System.out.println(triangleType(1, 1, 1));
+//    System.out.println(triangleType(2147483647, 2147483647, 2147483647));
+//    System.out.println(triangleType(3, 3, 5));
+//    System.out.println(triangleType(10, 10, 5));
+//    System.out.println(triangleType(15, 1, 15));
+//    System.out.println(triangleType(3, 4, 5));
+//    System.out.println(triangleType(5, 3, 4));
+//    System.out.println(triangleType(4, 5, 3));
+//    System.out.println(triangleType(2147483647, 2147483647, 2147483647));
+//
+//
 //    System.out.println(reverseWords("this is a sentence"));
 //    System.out.println(reverseWord("pumpernickel"));
+//
+//
 //  }
 }
--- src/main/java/io/swagger/api/ApiApiController.java
+++ src/main/java/io/swagger/api/ApiApiController.java
@@ -30,35 +30,6 @@ public class ApiApiController implements ApiApi {
 
     public ResponseEntity<String> tokenGet() {
       HttpHeaders responseHeaders = new HttpHeaders();
-//      responseHeaders.setCacheControl("no-cache");
-//      responseHeaders.setPragma("no-cache");
-//      responseHeaders.setContentType(MediaType.APPLICATION_JSON);
-
-//      {
-//        "pragma": "no-cache",
-//          "date": "Fri, 06 Jan 2017 03:54:29 GMT",
-//          "content-encoding": "gzip",
-//          "server": "Microsoft-IIS/8.0",
-//          "x-aspnet-version": "4.0.30319",
-//          "x-powered-by": "ASP.NET",
-//          "vary": "Accept-Encoding",
-//          "content-type": "application/json; charset=utf-8",
-//          "cache-control": "no-cache",
-//          "content-length": "130",
-//          "expires": "-1"
-//      }
-
-//      {
-//        "date": "Fri, 06 Jan 2017 03:57:34 GMT",
-//          "via": "1.1 vegur",
-//          "server": "Apache-Coyote/1.1",
-//          "connection": "keep-alive",
-//          "content-length": "36",
-//          "content-type": "text/json;charset=UTF-8"
-//      }
-      //responseHeaders.set("pragma", "no-cache");
-      //responseHeaders.set("content-encoding", "gzip");
-
       String token = "\"03c5e92c-292d-45f6-a74b-dc7981f6d821\"";
       return new ResponseEntity<String>(token, HttpStatus.OK);
     }
